khadas/android_packages_apps_Camera2

commit 92d2e202a410a855c4c48b343fb4f2b5bbfe2cf2

Camera: Merge from Android T
Change-Id: If5c7b1e71f2ec61f2fc1a70740318adf39616838
Signed-off-by: jinfeng.han <jinfeng.han@rock-chips.com>

src/com/android/camera/one/v2/Camera2OneCameraManagerImpl.java
@@ -173,10 +173,9 @@ public class Camera2OneCameraManagerImpl extends CameraManager.AvailabilityCallb
         String cameraId = findFirstCameraIdFacing(CameraCharacteristics.LENS_FACING_BACK);
         if (cameraId == null) {
             Log.w(TAG, "No back-facing camera found.");
-            cameraId = findFirstCameraIdFacing(CameraCharacteristics.LENS_FACING_EXTERNAL);
-            if (cameraId == null) {
-                Log.w(TAG, "No external camera found.");
-            }
+            Log.d(TAG, "Getting First FRONT Camera");
+            //xcq add
+            cameraId = findFirstCameraIdFacing(CameraCharacteristics.LENS_FACING_FRONT);
         }
         return cameraId;
     }
@@ -186,7 +185,7 @@ public class Camera2OneCameraManagerImpl extends CameraManager.AvailabilityCallb
         Log.d(TAG, "Getting First FRONT Camera");
         String cameraId = findFirstCameraIdFacing(CameraCharacteristics.LENS_FACING_FRONT);
         if (cameraId == null) {
-            Log.w(TAG, "No front-facing camera found.");
+            Log.w(TAG, "No front-facing camera found,try to find external facing camera.");
             cameraId = findFirstCameraIdFacing(CameraCharacteristics.LENS_FACING_EXTERNAL);
             if (cameraId == null) {
                 Log.w(TAG, "No external camera found.");

commit 6698ba3626d5490664bcc1b2fa087c87664b82f1

Fix external camera support missing caused by commit 92d2e202a
Type: Fix
Redmine ID: 484004
Associated modifications: N/A
Test: N/A

The issue commit info:
    92d2e202a Camera: Merge from Android T

Change-Id: I9dc8c8138456d2a476bee74c8a4e6a7467b25e8a
Signed-off-by: Cody Xie <cody.xie@rock-chips.com>

src/com/android/camera/one/v2/Camera2OneCameraManagerImpl.java
@@ -182,6 +182,13 @@ public class Camera2OneCameraManagerImpl extends CameraManager.AvailabilityCallb
             Log.d(TAG, "Getting First FRONT Camera");
             //xcq add
             cameraId = findFirstCameraIdFacing(CameraCharacteristics.LENS_FACING_FRONT);
+            if (cameraId == null) {
+                Log.w(TAG, "No front-facing camera found,try to find external facing camera.");
+                cameraId = findFirstCameraIdFacing(CameraCharacteristics.LENS_FACING_EXTERNAL);
+                if (cameraId == null) {
+                    Log.w(TAG, "No external camera found.");
+                }
+            }
         }
         return cameraId;
     }